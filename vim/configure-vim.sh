28241008a595a32099de580e6ce20c4bfe81e804
Matthew R Kasun <mkasun@nusak.ca>
2022-02-05 15:13:55 +0000

    fix script

--- vim/configure-vim.sh
+++ vim/configure-vim.sh
@@ -1,13 +1,13 @@
-#!/user/bin/env bin
+#!/usr/bin/env bash
 
 mkdir -p ~/.vim/pack/themes/start
 mkdir -p ~/.vim/pack/dist/start
 
 cp .vimrc ~/.vimrc
 
-git clone https://github.com/dracula/vim -o ~/.vim/pack/themes/start/dracula
-git clone https://github.com/fatih/vim-go -o ~/.vim/pack/dist/start/vim-go
-git clone https://github.com/tpope/vim-fugitive -o ~/.vim/pack/dist/start/vim-fugitive
-git clone https://github.com/vim-airlin/vim-airline -o ~/.vim/pack/dist/start/vim-airline
-git clone https://github.com/preservim/tagbar -o ~/.vim/pack/dist/start/tagbar
+git clone https://github.com/dracula/vim  ~/.vim/pack/themes/start/dracula
+git clone https://github.com/fatih/vim-go  ~/.vim/pack/dist/start/vim-go
+git clone https://github.com/tpope/vim-fugitive  ~/.vim/pack/dist/start/vim-fugitive
+git clone https://github.com/vim-airline/vim-airline  ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/preservim/tagbar  ~/.vim/pack/dist/start/tagbar
 
